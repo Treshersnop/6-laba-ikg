e59b7678f77c38abaa2e2518ece977da1c4edc4d
Treshersnop <94636250+Treshersnop@users.noreply.github.com>
2022-05-28 12:48:16 +0500

    pic

---
+++ 42/atb.h
@@ -0,0 +1,30 @@
+#ifndef OGLDEV_ATB_H
+#define	OGLDEV_ATB_H
+
+#include "keys.h"
+#include "math_3d.h"
+
+#include "AntTweakBar.h"
+
+class ATB
+{
+public:
+    ATB();
+
+    bool Init();
+
+    bool KeyboardCB(OGLDEV_KEY OgldevKey);
+
+    bool PassiveMouseCB(int x, int y);
+
+    void RenderSceneCB();
+
+    bool MouseCB(OGLDEV_MOUSE Button, OGLDEV_KEY_STATE State, int x, int y);
+
+};
+
+
+extern TwType TW_TYPE_OGLDEV_VECTOR3F;
+extern TwType TW_TYPE_OGLDEV_ATTENUATION;
+
+#endif
